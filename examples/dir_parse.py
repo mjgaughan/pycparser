e09b2c4aa84e671f47eb2b5fa0fbff6599f14b9e
mgaughan <mgaughan@middlebury.edu>
2022-01-15 15:49:03 -0500

    troubleshooting compiling linux, parser good

--- examples/dir_parse.py
+++ examples/dir_parse.py
@@ -6,13 +6,16 @@ import func_defs
 
 
 def recurse_down(root_dir):
+    print(root_dir)
+    print(os.getcwd())
     for filename in glob.glob(root_dir + '**/*.c', recursive=True):
         print(filename)
+        
         try:
             func_defs.show_func_defs(filename)
         except BaseException:
             print("oops! error in %s", filename)
-
+        
 
 if __name__ == '__main__':
-    recurse_down('../')
+    recurse_down('linux/')
